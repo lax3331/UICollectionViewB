e0582a5f25b38571a2b0c46cb5cc4fda12303f55
Nikky <la2life1234@gmail.com>
2023-03-30 10:17:39 +0300

    Initial Commit

---
+++ UICollectionViewB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionViewB
+//
+//  Created by Nikita Kuznetsov on 30.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
